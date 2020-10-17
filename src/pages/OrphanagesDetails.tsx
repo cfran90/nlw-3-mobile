d34ea099b18b6c71ed5a08bba895cb5d2b938d7c
Cesar Frantz <cesarfrantz@animati.com.br>
2020-10-17 14:39:43 -0300

    Quarto dia: nlw3! Iniciando aplicação mobile com react native. Adicionando mapa e navegação.

---
+++ src/pages/OrphanagesDetails.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {StyleSheet, View} from "react-native";
+
+export default function OrphanagesDetails() {
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+
+});
